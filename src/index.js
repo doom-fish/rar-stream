f8fdfacc2655baff16e5f73ea10e560cef954b39
Per <mail@perjohansson.net>
2016-09-18 22:44:45 +0200

    fixed dependencies

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import path from 'path';
 import LocalFileMedia from './file-media/local-file-media';
 import RarFileBundle from './rar-file/rar-file-bundle';
 import RarManifest from './rar-manifest/rar-manifest';
+import progressStream from 'progress-stream';
+import prettysize from 'prettysize';
 
 const directory = process.argv[2];
 
@@ -16,8 +18,16 @@ fs.readdir(directory, (err, files) => {
 
   manifest.getFiles().then((innerFiles) => {
     innerFiles.forEach(innerFile => {
+      const str = progressStream({
+          length: innerFile.size,
+          time: 100
+      });
+      str.on('progress', (progress) => {
+        process.stdout.write('\x1B[2J\x1B[0f');
+        console.log(`Unpacking: ${innerFile.name} (${Math.round(progress.percentage)}%) at speed: ${prettysize(progress.speed)}/S`)
+      });
       const writeStream = fs.createWriteStream(path.resolve(directory, innerFile.name));
-      innerFile.createReadStream(0, innerFile.size - 1).pipe(writeStream);
+      innerFile.createReadStream(0, innerFile.size - 1).pipe(str).pipe(writeStream);
     });
   });
 });
