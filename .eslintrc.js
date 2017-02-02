375994ae6673c27788f65ca73779a6d429294e44
Per <mail@perjohansson.net>
2017-02-02 21:51:04 +0100

    Added useful eslint plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,12 @@ module.exports = {
         node: true
     },
     parser: 'babel-eslint',
-    plugins: ['prettier', 'babel', 'flowtype', 'filenames'],
-    extends: 'eslint:recommended',
+    plugins: ['import', 'ava', 'prettier', 'babel', 'flowtype', 'filenames'],
+    extends: [
+        'eslint:recommended',
+        'plugin:import/errors',
+        'plugin:import/warnings'
+    ],
     parserOptions: {
         sourceType: 'module'
     },
@@ -13,6 +17,30 @@ module.exports = {
         indent: ['error', 4],
         'linebreak-style': ['error', 'unix'],
         quotes: ['error', 'single'],
-        semi: ['error', 'always']
+        semi: ['error', 'always'],
+        'ava/assertion-arguments': 'error',
+        'ava/max-asserts': ['off', 5],
+        'ava/no-async-fn-without-await': 'error',
+        'ava/no-cb-test': 'off',
+        'ava/no-duplicate-modifiers': 'error',
+        'ava/no-identical-title': 'error',
+        'ava/no-ignored-test-files': 'error',
+        'ava/no-invalid-end': 'error',
+        'ava/no-nested-tests': 'error',
+        'ava/no-only-test': 'error',
+        'ava/no-skip-assert': 'error',
+        'ava/no-skip-test': 'error',
+        'ava/no-statement-after-end': 'error',
+        'ava/no-todo-implementation': 'error',
+        'ava/no-todo-test': 'warn',
+        'ava/no-unknown-modifiers': 'error',
+        'ava/prefer-async-await': 'error',
+        'ava/prefer-power-assert': 'off',
+        'ava/test-ended': 'error',
+        'ava/test-title': ['error', 'if-multiple'],
+        'ava/use-t-well': 'error',
+        'ava/use-t': 'error',
+        'ava/use-test': 'error',
+        'ava/use-true-false': 'error'
     }
 };
